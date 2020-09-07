968009674fd3597847ee7c9ee5d322b7e9cc7795
Carlos San Bartolomé <carlos.sanbs@gmail.com>
2020-09-07 00:51:42 -0500

    DONE gamarrita backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2fb602000fbaf6dd3af5830cc31bd298'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '79e7148a-995e-4c9f-9107-cb01f5443e52'
+};
